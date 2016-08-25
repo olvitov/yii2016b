6c93f4d38ba3396c9ae1212345de2c16b81aa5ef
olvit <olvitov@gmail.com>
2016-08-25 22:29:03 +0300

    Routing

--- config/web.php
+++ config/web.php
@@ -10,6 +10,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'r95-EhqsU0HRmj35IsHl-vRHQw3-dO4H',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -38,14 +39,20 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',
+                '<action>'=>'site/<action>',
+                '<controller:\w+>/<id:\d+>'
+                => '<controller>/view',
+                '<controller:\w+>/<action:\w+>'
+                => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>/<id:\d+>'
+                => '<controller>/<action>',
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
